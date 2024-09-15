aca98f184f2bb2b1e6516f7736fd25969424a082
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-09-15 23:14:12 +0530

    all logic deleted

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -10,25 +10,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import passwordStore from "@/context/password";
-import userStore from "@/context/login";
 
 export default function Password() {
-    const [passwordState, setPasswordState] = useState('');
-    const [confirmPasswordState, setConfirmPasswordState] = useState('');
-    const { setPassword } = passwordStore();
-    const { isLoggedIn, LogIn } = userStore();
-
-    function SubmitPassword() {
-        if (passwordState === confirmPasswordState) {
-            setPassword(passwordState);
-            localStorage.setItem('app-password', passwordState);
-        } else {
-            alert('Passwords do not match'); 
-        }
-    }
-
     return (
         <div className="w-full h-full min-h-screen flex justify-center items-center bg-background">
             <Card className="w-1/4 h-2/3 border-border bg-popover">
@@ -36,14 +19,7 @@ export default function Password() {
                     <CardTitle className="text-3xl font-bold">Pocket</CardTitle>
                     <CardDescription>Enter the local password for the wallet</CardDescription>
                 </CardHeader>
-                {isLoggedIn ?
-                <CardContent className="flex flex-col gap-4">  
-                    <div className="grid w-full max-w-sm items-center gap-1.5">
-                        <Label htmlFor="password">Password</Label>
-                        <Input id="password" value={passwordState} onChange={(e) => setPasswordState(e.target.value)} type="password" />
-                    </div>
-                </CardContent>
-                :<CardContent className="flex flex-col gap-4">
+                <CardContent className="flex flex-col gap-4">
                     <div className="grid w-full max-w-sm items-center gap-1.5">
                         <Label htmlFor="password">Password</Label>
                         <Input id="password" value={passwordState} onChange={(e) => setPasswordState(e.target.value)} type="password" />
@@ -52,9 +28,7 @@ export default function Password() {
                         <Label htmlFor="confirmPassword">Confirm Password</Label>
                         <Input id="confirmPassword" value={confirmPasswordState} onChange={(e) => setConfirmPasswordState(e.target.value)} type="password" />
                     </div>
-                </CardContent>
-                }
-                
+                </CardContent>                
                 <CardFooter>
                     <Button className="w-full" onClick={SubmitPassword}>Submit</Button>
                 </CardFooter>
--- src/app/secret-pharse/page.tsx
+++ src/app/secret-pharse/page.tsx
@@ -1,78 +1,51 @@
 'use client';
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { generateMnemonic, mnemonicToSeedSync } from "bip39";
-import { useEffect, useState } from "react";
-import useStore from "@/context/login";
-import { useToast } from "@/hooks/use-toast";
 
-function SecretPharse() {
-    const [mnemonic, setMnemonic] = useState('');
-    const [checked, setChecked] = useState(false);
-    const {setSeed} = useStore()
-    const { toast } = useToast()
-
-    useEffect(() => {
-        const generatedMnemonic = generateMnemonic();
-        setMnemonic(generatedMnemonic);
-    }, []);
-
-    const words = mnemonic.split(' ');
-
-    async function CopyPhrase() {
-        try {
-            await navigator.clipboard.writeText(mnemonic);
-            toast({
-                title: "Phrase Copied",
-                description: "Phrase copied to clipboard",
-            })
-        } catch (error) {
-            console.error('Failed to copy phrase:', error);
-        }
-    }
-
-    function SubmitPhrase() {
-        const seed = mnemonicToSeedSync(mnemonic);
-        setSeed(seed.toString('hex'));
-        const user = {
-            isLoggedIn: true,
-            seed: seed.toString('hex'),
-        }
-        localStorage.setItem('app-user', JSON.stringify(user));
-    }
-
-    return (
-        <div className='w-full h-full min-h-screen flex justify-center items-center text-primary bg-background'>
-            <div className='max-w-[1080px] w-full flex flex-col items-center'>
-                <div className="flex flex-col items-center gap-2">
-                    <h1 className='text-5xl text-center font-bold'>Secret Phrase</h1>
-                    <p className='text-secondary-foreground text-center'>Save these words in a Safe Place</p>
-                    <h3 className='text-blue-400 text-xl text-center'>Read the Warning again</h3>
-                </div>
-                <div onClick={CopyPhrase} className='bg-muted p-2 w-full max-w-[60%] rounded-md space-y-3 cursor-pointer'>
-                    <div className='grid grid-cols-3 gap-2'>
-                        {words.map((word, index) => (
-                            <div key={index} className='p-2 border border-ring rounded-[0.5rem]'>
-                                {index + 1}. {word}
-                            </div>
-                        ))}
-                    </div>
-                    <div className='border-t text-center border-ring w-full'>Click anywhere on this card to copy</div>
-                </div>
-                <div className='mt-4'>
-                    <div className="flex items-center space-x-2">
-                        <Checkbox id="terms2" checked={checked} onCheckedChange={(value) => setChecked(value === 'indeterminate' ? false : value)} />
-                        <label htmlFor="terms2" className="text-sm font-medium leading-none">
-                            I saved the phrase in a safe place
-                        </label>
-                    </div>
-                    <Button onClick={SubmitPhrase} className="w-full mt-4" disabled={!checked}>
-                        Next
-                    </Button>
-                </div>
-            </div>
+const SecretPhrase = () => {
+  return (
+    <div className="w-full h-full min-h-screen flex justify-center items-center text-primary bg-background">
+      <div className="max-w-[1080px] w-full flex flex-col items-center">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-5xl text-center font-bold">Secret Phrase</h1>
+          <p className="text-secondary-foreground text-center">
+            Save these words in a Safe Place
+          </p>
+          <h3 className="text-blue-400 text-xl text-center">Read the Warning again</h3>
         </div>
-    );
-}
+        <div
+          onClick={copyPhrase}
+          className="bg-muted p-2 w-full max-w-[60%] rounded-md space-y-3 cursor-pointer"
+        >
+          <div className="grid grid-cols-3 gap-2">
+            {words.map((word, index) => (
+              <div key={index} className="p-2 border border-ring rounded-[0.5rem]">
+                {index + 1}. {word}
+              </div>
+            ))}
+          </div>
+          <div className="border-t text-center border-ring w-full">
+            Click anywhere on this card to copy
+          </div>
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center space-x-2">
+            <Checkbox
+              id="terms2"
+              checked={checked}
+              onCheckedChange={(value) => setChecked(value == "checked")}
+            />
+            <label htmlFor="terms2" className="text-sm font-medium leading-none">
+              I saved the phrase in a safe place
+            </label>
+          </div>
+          <Button onClick={submitPhrase} className="w-full mt-4" disabled={!checked}>
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SecretPharse;
+export default SecretPhrase;
